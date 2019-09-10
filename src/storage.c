e4e910c9c2a36147c5b5fa45da1545b49249bfee
adrianmiriuta <adrian.miriuta@gmail.com>
2019-09-10 08:55:14 +0200

    cleanup

--- src/storage.c
+++ src/storage.c
@@ -1,22 +1,3 @@
-/*
-    Copyright 2016 fishpepper <AT> gmail.com
-
-    This program is free software: you can redistribute it and/ or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http:// www.gnu.org/licenses/>.
-
-    author: fishpepper <AT> gmail.com
-*/
-
 #include "storage.h"
 #include "debug.h"
 #include "wdt.h"
@@ -166,7 +147,7 @@ static void storage_load_defaults(void) {
 
     // add example model
     storage_mode_set_name(0, "OpenGround");
-    storage.model[0].timer = 10*60;
+    storage.model[0].timer = 8*60;
     storage.model[0].stick_scale = 100;
     storage.current_model = 0;
 }
