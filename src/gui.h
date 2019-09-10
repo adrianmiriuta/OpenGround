3323268128bbfea5af566b0ffb37cee21a391450
adrianmiriuta <adrian.miriuta@gmail.com>
2019-09-10 09:25:37 +0200

    cleanup

--- src/gui.h
+++ src/gui.h
@@ -1,24 +1,4 @@
-/*
-    Copyright 2016 fishpepper <AT> gmail.com
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    author: fishpepper <AT> gmail.com
-*/
-
-#ifndef GUI_H_
-#define GUI_H_
+#pragma once
 
 #include <stdint.h>
 #include "adc.h"
@@ -79,6 +59,3 @@ void gui_init(void);
 void gui_loop(void);
 
 uint32_t gui_running(void);
-
-
-#endif  // GUI_H_
